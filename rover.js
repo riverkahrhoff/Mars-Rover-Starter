fb9663d7727506002f38b53ab8ccc8311792df86
River Kahrhoff <river.kahrhoff@gmail.com>
2024-07-29 09:36:01 -0500

    complete and working assignmnet 3

--- rover.js
+++ rover.js
@@ -7,6 +7,7 @@ class Rover {
       this.mode = "NORMAL";
       this.generatorWatts = 110;
     };
+
     receiveMessage(message) {
 
       let resultsArray = [];
@@ -16,76 +17,44 @@ class Rover {
          results: resultsArray
       };
 
-      let roverStatus = {
-         completed: true,
-         roverStatus: { 
-            mode: this.mode,
-            generatorWatts: this.generatorWatts,
-            position: this.position,
-         }
-      };
-
-      let roverMode = {
-         completed: true
-      }
-
-      let moveRover = {
-         completed: true
-      }
 
 
       for (let i = 0; i < message.commands.length; i++) {
-         let newCommand = message.commands[i].commandType
-
-         let roverMode = {
-            completed: true
-         }
-   
-         let moveRover = {
-            completed: true
-         }
+         let command = message.commands[i];
+         let newCommand = command.commandType
    
          if (newCommand == "MOVE") {
-            if (roverStatus.roverStatus.mode == "LOW_POWER") {
-               moveRover.completed = false
-               resultsArray.push(moveRover)
+            if (this.mode === "LOW_POWER") {
+               resultsArray.push({completed: false})
             } else {
-               moveRover.completed = true
-               roverStatus.roverStatus.position = message.commands[i].value
-               resultsArray.push(moveRover)
+               this.position = command.value
+               resultsArray.push({completed: true})
             }
 
-         } else if (newCommand == "MODE_CHANGE") {
-            if (message.commands[i].value === "NORMAL") {
-               roverStatus.roverStatus.mode = "NORMAL"
-               resultsArray.push(roverMode)
-            } else if (message.commands[i].value === "LOW_POWER"){
-               roverStatus.roverStatus.mode = "LOW_POWER"
-               resultsArray.push(roverMode)
+         } 
+
+         else if (newCommand === "STATUS_CHECK"){
+            let roverStatus = {
+               completed: true,
+               roverStatus: { 
+                  mode: this.mode,
+                  generatorWatts: this.generatorWatts,
+                  position: this.position,
+               }
+            };
+            resultsArray.push(roverStatus)
+
+         }else if (newCommand == "MODE_CHANGE") {
+               this.mode = command.value;
+               resultsArray.push({completed: true})
             }
          
 
-         } else if (newCommand == "STATUS_CHECK"){
-            resultsArray.push(roverStatus)
-
-         }
+         } 
+         return returnObject;
       }
-      return returnObject;
-      };
       };
 
 
-      let rover = new Rover(100);
-      let commands = [
-         new Command('STATUS_CHECK'),
-         new Command('MOVE', 4321),
-         new Command('STATUS_CHECK'),
-         new Command('MODE_CHANGE', 'LOW_POWER'),
-         new Command('MOVE', 3579)
-      ];
-      let message = new Message('TA power', commands);
-      let response = rover.receiveMessage(message);
-      
-      console.log(response.results);
 
 module.exports = Rover;
